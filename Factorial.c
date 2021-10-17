06093178c58d752efc3472abc405ce4a454154c3
sophia-manole <91803712+sophia-manole@users.noreply.github.com>
2021-10-17 16:30:37 -0700

    Update Factorial.c
    I made a few corrections to the code to make sure that it runs smoothly. At the beginning, the number that was entered was stored in "fac," but in line 11 the number 2 was put into fac. So, I made the number stored in "no." I also made fac=1 because you need to start at 1 when multiplying numbers, not at 2. On line 15, "w" was never used down below, so I replaced it with "no." There was also no semi-colon at the end of line 18, so I added that.

--- Factorial.c
+++ Factorial.c
@@ -4,17 +4,19 @@ int main(void)
 {
  int no;
  long fac;
-
+ 
  cout << "Enter a Number : ";
- cin >> fac;
+ cin >> no;
+ 
+ 
 
- fac = 2;
+ fac = 1;
 
   for (int r=no; r >= 1; r--)
   {
-    fac = fac * w;
+    fac = fac * no;
   }
 
- cout << "Factorial of " << no <<" is " fac <<endl 
+ cout << "Factorial of " << no <<" is " fac <<endl;
  
 }
